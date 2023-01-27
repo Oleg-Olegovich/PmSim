106eb725949112baec5d882648a712a2cd85ffb9
Olek_Olegovich <olek.olegovich@gmail.com>
2023-01-27 22:24:21 +0300

    Cross platform client app created.

---
+++ Frontend/App/PmSim.Frontend.App/PmSim.Frontend.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PmSim.Frontend.App.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Frontend/App/PmSim.Frontend.App/PmSim.Frontend.App/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PmSim.Frontend.App.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
